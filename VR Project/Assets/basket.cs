49d358aeef001f2a40c275934493cce580e95789
Nadine Gutiérrez <36292059+Nadine044@users.noreply.github.com>
2022-06-27 22:36:52 +0200

    Books Added

--- VR Project/Assets/basket.cs
+++ VR Project/Assets/basket.cs
@@ -11,11 +11,22 @@ public class basket : MonoBehaviour
     public Animator bars;
     public Animator wood;
 
-    private bool acitveBars;
+    public GameObject ball1;
+    public GameObject ball2;
+    public GameObject ball3;
+    public GameObject ball4;
+
+    private Vector3 initial_pos1;
+    private Vector3 initial_pos2;
+    private Vector3 initial_pos3;
+    private Vector3 initial_pos4;
 
     private void Start()
     {
-        acitveBars = false;
+        initial_pos1 = ball1.transform.position;
+        initial_pos2 = ball2.transform.position;
+        initial_pos3 = ball3.transform.position;
+        initial_pos4 = ball4.transform.position;
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -43,5 +54,13 @@ public class basket : MonoBehaviour
         yield return new WaitForSeconds(2.0f);
         bars.SetBool("barsDown", false);
         wood.SetBool("play", false);
+    }
+
+    public void ReStartBallsPosition()
+    {
+        ball1.transform.position = initial_pos1;
+        ball2.transform.position = initial_pos2;
+        ball3.transform.position = initial_pos3;
+        ball4.transform.position = initial_pos4;
     }
 }
